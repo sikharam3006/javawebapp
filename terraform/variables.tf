72e085bef9c510c9f6c5656b4f5b04b08049743f
sikharam3006 <susmitha.sikharam30@gmail.com>
2025-12-15 19:57:48 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,5 +3,5 @@ default = "Canada Central"
 }
 
 
-variable "resource_group_name" { default = "rg"}
+variable "resource_group_name" {  default = "rg"}
 variable "app_service_name" { default= "java-app-service-dev"}
