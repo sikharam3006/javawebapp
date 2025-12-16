dac0ff887f0d4af79fbeaae7359f0bb7e4a55479
sikharam3006 <susmitha.sikharam30@gmail.com>
2025-12-16 14:51:55 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,17 +17,17 @@ sku_name = "B1"
 
 
 resource "azurerm_linux_web_app" "app" {
-name = var.app_service_name
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
-service_plan_id = azurerm_service_plan.plan.id
+  name                = var.app_service_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.plan.id
 
+  site_config {
+    application_stack {
+      java_version        = "17"
+      java_server         = "TOMCAT"
+      java_server_version = "9.0"
+    }
+  }
+}
 
-site_config {
-application_stack {
-java_version = "17"
-java_server         = "TOMCAT"
-java_server_version = "9.0
-}
-}
-}
