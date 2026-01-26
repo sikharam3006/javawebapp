0e35bbf58414f50b2156f7135c24c9c7ba6f1f1e
sikharam3006 <susmitha.sikharam30@gmail.com>
2026-01-26 16:00:16 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
 backend "azurerm" {
-resource_group_name = "tfstate-rg"
+resource_group_name = "java_demo"
 storage_account_name = "tfstateaccount9090"
 container_name = "tfstate"
 key = "javaapp.tfstate"
